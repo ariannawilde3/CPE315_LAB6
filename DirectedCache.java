3c5dc6dc5173b03cb643275d0cb99cf9b698f1c0
Arianna <arianna@wildefamily.net>
2024-06-05 21:04:13 -0700

    initial

---
+++ DirectedCache.java
@@ -0,0 +1,22 @@
+
+/*
+ *Directed Cache: is like a row of lockers with specific locker numbers
+ each locker can hold one unique item with a unique item ID
+ to find where it is placed you do add / id %2
+ if there is a collision replace item 
+
+ 2KB, direct mapped, 1-word blocks
+ 2KB, direct mapped, 2-word blocks
+ 2KB, direct mapped, 4-word blocks
+ 4KB, direct mapped, 1-word blocks
+
+Use the following 2 data files for your testing.  Each file contains 5 million memory reference addresses taken from actual running programs.  
+
+Each line of the file contains a single 32-bit memory reference address.
+Work with each address as an integer - (25% grade deduction - DO NOT store addresses as strings, array of chars, or array of individual bits)
+Your code should be reasonably fast (less than 10-15 seconds on your local computer)
+
+ */
+public class DirectedCache {
+    
+}
